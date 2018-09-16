fb5b6b155531d9c990a7c3828bb9a42069c40a2c
Jim Beck <jamesbeckjr@gmail.com>
2018-09-15 20:37:22 -0400

    Created Balloon Popping Demo
    Balloon grows whenever the player clicks on it.
    The balloons is destroyed when the scale goes over 3

---
+++ Balloon Popper/Assets/Scripts/BalloonController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class BalloonController : MonoBehaviour
+{
+    [SerializeField]
+    private float scaleFactor = 1.2f;
+
+    private float maxScale = 3f;
+
+    private void Start()
+    {
+        if(scaleFactor <= 1)
+        {
+            print("The scale factor value is too small");
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        transform.localScale *= scaleFactor;
+
+        if(transform.localScale.x >= maxScale)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
